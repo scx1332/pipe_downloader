2c9025430d9a2692961996b6b569beec559f7323
scx1332 <sieciech@gmail.com>
2022-09-23 15:17:13 +0200

    run spawn

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use std::str::FromStr;
 use reqwest::blocking::Client;
 use anyhow;
 use lz4::{Decoder, EncoderBuilder};
-use std::io::BufReader;
+use std::io::{BufReader, Cursor, Read};
+use std::ptr::addr_of_mut;
+use std::sync::mpsc::channel;
+use std::thread;
+use log;
 
 struct PartialRangeIter {
     start: u64,
@@ -50,9 +54,83 @@ fn decompress(source: &Path, destination: &Path) -> Result<()> {
     Ok(())
 }*/
 
+
+struct Pipe {
+    pos: usize,
+    file_size: usize,
+    receiver: std::sync::mpsc::Receiver<Vec<u8>>,
+    current_buf: Vec<u8>,
+    current_buf_pos: usize,
+}
+
+impl Read for Pipe {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let starting_pos = self.pos;
+        if self.current_buf.is_empty() || self.current_buf_pos >= self.current_buf.len() {
+            let res = self.receiver.recv();
+            if res.is_err() {
+                return Ok(0);
+            }
+            self.current_buf = res.unwrap();
+            self.current_buf_pos = 0;
+        }
+        let min_val = std::cmp::min(self.current_buf.len() - self.current_buf_pos, buf.len());
+        for i in 0..min_val {
+            buf[i] = self.current_buf[self.current_buf_pos];
+            self.current_buf_pos += 1;
+            self.pos += 1;
+        }
+        log::debug!("Chunk read: starting_pos: {} / length: {}", starting_pos, self.pos - starting_pos);
+        return Ok(min_val);
+
+        let mut bytes_read: usize = 0;
+        for i in 0..buf.len() {
+            if self.pos >= self.file_size {
+                break;
+            }
+            self.pos += 1;
+            buf[i] = char::from_digit((i % 10) as u32, 10).unwrap() as u8;
+            bytes_read += 1;
+        }
+        Ok(bytes_read)
+    }
+}
+
+
+fn download_chunk(url: &str, client: reqwest::blocking::Client, range: std::ops::Range<usize>) -> anyhow::Result<Vec<u8>> {
+    log::debug!("Downloading chunk: range {:?} / {}", range, range.end - range.start);
+
+    let header = format!("bytes={}-{}", range.start, range.end - 1);
+    let mut response = client.get(url).header("Range", header).send()?;
+
+    let status = response.status();
+    let content_length = response.headers().get("Content-Length").ok_or(anyhow::anyhow!("Content-Length header not found"))?.to_str()?;
+    let content_length = usize::from_str(content_length)?;
+
+    if !(status == StatusCode::OK || status == StatusCode::PARTIAL_CONTENT) {
+        anyhow::anyhow!("unexpected status code: {}", status);
+    } else {
+        log::info!("Chunk downloaded with status: {:?}", status);
+    }
+    let mut buf_vec: Vec<u8> = vec![];
+    //let mut file = Cursor::new(buf_vec);
+    //std::io::copy(&mut response, &mut file).unwrap();
+    response.read_to_end(&mut buf_vec)?;
+
+    assert!(buf_vec.len() == range.end - range.start);
+    log::debug!("Chunk downloaded: range {:?} / {}", range, range.end - range.start);
+
+    return Ok(buf_vec);
+
+    //std::io::copy(&mut response, &mut output_file)?;
+}
+
 fn main() -> anyhow::Result<()> {
-    let url = "http://mumbai-main.golem.network:14372/beacon.tar.lz4";
-    const CHUNK_SIZE: u32 = 50000000;
+    env_logger::init();
+
+    //let url = "http://mumbai-main.golem.network:14372/beacon.tar.lz4";
+    let url = "https://github.com/golemfactory/ya-runtime-http-auth/releases/download/v0.1.0/ya-runtime-http-auth-linux-v0.1.0.tar.gz";
+    const CHUNK_SIZE: usize = 50000;
 
     let client = reqwest::blocking::Client::new();
     let response = client.head(url).send()?;
@@ -60,12 +138,13 @@ fn main() -> anyhow::Result<()> {
         .headers()
         .get(CONTENT_LENGTH)
         .ok_or("response doesn't include the content length").unwrap();
-    let length = u64::from_str(length.to_str()?).map_err(|_| "invalid Content-Length header").unwrap();
+    let length = usize::from_str(length.to_str()?).map_err(|_| "invalid Content-Length header").unwrap();
 
     let mut output_file = File::create("download.bin")?;
 
     println!("starting download...");
-    for range in PartialRangeIter::new(0, length - 1, CHUNK_SIZE)? {
+    let (send, recv) = channel();
+    /*for range in PartialRangeIter::new(0, length - 1, CHUNK_SIZE)? {
         println!("range {:?} / {}", range, length);
         let mut response = client.get(url).header(RANGE, range).send()?;
 
@@ -74,10 +153,32 @@ fn main() -> anyhow::Result<()> {
             anyhow::anyhow!("unexpected status code: {}", status);
         }
         std::io::copy(&mut response, &mut output_file)?;
-    }
+    }*/
 
-    let content = response.text()?;
-    std::io::copy(&mut content.as_bytes(), &mut output_file)?;
+    thread::spawn(move || {
+        for i in 0..(length / CHUNK_SIZE + 1) {
+            let max_length = std::cmp::min(CHUNK_SIZE, length - i * CHUNK_SIZE);
+            if max_length == 0 {
+                break;
+            }
+            let range = std::ops::Range {
+                start: i * CHUNK_SIZE,
+                end: i * CHUNK_SIZE + max_length,
+            };
+            let client = reqwest::blocking::Client::new();
+
+            let current_buf = download_chunk(url, client, range).unwrap();
+            send.send(current_buf).unwrap();
+        }
+
+    });
+    let mut p = Pipe { pos: 0, file_size: length, receiver: recv, current_buf: vec![], current_buf_pos: 0 };
+    //while let Ok(current_buf) = recv.recv() {
+    std::io::copy(&mut p, &mut output_file)?;
+    //}
+    //let mut br = BufReader::new(p);
+    //let content = response.text()?;
+    //std::io::copy(&mut content.as_bytes(), &mut output_file)?;
 
     println!("Finished with success!");
     Ok(())
